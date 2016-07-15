efe574e9858049522fb23e49752410107e1f5923
lastromanticx <giladbarkan@live.com>
2016-07-15 11:51:38 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,30 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(artist_name)
+    self.artist = Artist.find_or_create_by(name: artist_name)
+  end 
+
+  def artist_name
+    self.try(:artist).try(:name)
+  end
+
+  def genre_name=(genre_name)
+    self.genre = Genre.find_or_create_by(name: genre_name)
+  end
+
+  def genre_name
+    self.try(:genre).try(:name)
+  end
+
+  def note_contents=(notes)
+    notes.each do |note|
+      self.notes.build(content: note) if !note.match(/^\s*$/)
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  end
+end
